de373068b7095c893d67dfed239f9b20e25b8203
EvgenyMeltsaykin <e.meltsaykin@napoleonit.ru>
2021-07-25 19:52:54 +0500

    Рефакторинг

--- app/src/main/java/com/poke/bulbazavr/di/Dagger.kt
+++ app/src/main/java/com/poke/bulbazavr/di/Dagger.kt
@@ -3,19 +3,16 @@ package com.poke.bulbazavr.di
 import com.poke.bulbazavr.MainActivity
 import com.poke.bulbazavr.api.PokeApiService
 import com.poke.bulbazavr.feature.pokeListScreen.PokeListFragment
-import com.poke.bulbazavr.feature.pokeListScreen.PokeListPresenter
 import dagger.Component
 import dagger.Module
 import dagger.Provides
-import okhttp3.OkHttpClient
 import javax.inject.Singleton
 
 @Component(modules = [AppModule::class])
+@Singleton
 interface AppComponent {
     fun inject(pokeListFragment: PokeListFragment)
     fun inject(mainActivity: MainActivity)
-
-    val pokeApi:PokeApiService
 }
 
 @Module(includes = [NetworkModule::class, AppBindModule::class])
@@ -24,6 +21,7 @@ class AppModule
 @Module
 class NetworkModule {
     @Provides
+    @Singleton
     fun providePokeApiService():PokeApiService {
         return PokeApiService.create()
     }
