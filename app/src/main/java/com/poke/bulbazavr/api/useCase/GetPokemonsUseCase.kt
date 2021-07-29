27c97eefefbe7b9fb4b5092c7d457f8ed17f932a
EvgenyMeltsaykin <e.meltsaykin@napoleonit.ru>
2021-07-29 22:00:00 +0500

    Эксперименты с rxJava

--- app/src/main/java/com/poke/bulbazavr/api/useCase/GetPokemonsUseCase.kt
+++ app/src/main/java/com/poke/bulbazavr/api/useCase/GetPokemonsUseCase.kt
@@ -4,7 +4,7 @@ import com.poke.bulbazavr.api.PokeApiService
 import com.poke.bulbazavr.api.data.request.OffsetLimitRequest
 import com.poke.bulbazavr.api.data.responses.BaseResponse
 import com.poke.bulbazavr.data.Pokemon
-import io.reactivex.rxjava3.android.schedulers.AndroidSchedulers
+import io.reactivex.rxjava3.core.Observable
 import io.reactivex.rxjava3.schedulers.Schedulers
 import javax.inject.Inject
 
@@ -17,22 +17,22 @@ class GetPokemonsUseCase @Inject constructor(
         onSuccess: (BaseResponse<Pokemon>) -> Unit,
         onFailed: (Throwable) -> Unit
     ) {
-
-        pokeApiService.getPokemons(params.offset).subscribeOn(Schedulers.io())
-            .observeOn(AndroidSchedulers.mainThread())
-            .doOnSuccess { response ->
-                val pokemons: MutableList<Pokemon> = mutableListOf()
-                response.results.forEach { pokemon ->
-                    val infoPokemon = pokeApiService.getPokemon(pokemon.name)
-                    infoPokemon.doOnSuccess {
-                        pokemons.add(pokemon.copy(urlPhoto = it.sprites.backDefault))
-                    }
-                }
-                onSuccess(response.copy(results = pokemons))
+        pokeApiService.getPokemons(params.offset)
+            .map { response ->
+                onSuccess(
+                    response.copy(
+                        results = Observable.fromIterable(response.results)
+                            .concatMap { pokemon ->
+                                pokeApiService.getPokemon(pokemon.name).map {
+                                    pokemon.copy(urlPhoto = it.sprites.backDefault)
+                                }.toObservable()
+                            }
+                            .toList()
+                    )
+                )
             }
-            .doOnError {
-                onFailed(it)
-            }
+            .doOnError { onFailed(it) }
+            .subscribeOn(Schedulers.io())
 
     }
 }
